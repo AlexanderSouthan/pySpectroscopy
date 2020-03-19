25c7a7450d9a1aba5ca8b88bd6a04c45a0fe78ff
Southan <alexander.southan@igb.fraunhofer.de>
2020-03-20 00:18:14 +0100

    added multivariate calibration

--- hplc_calibration.py
+++ hplc_calibration.py
@@ -7,15 +7,13 @@ Created on Wed Mar 18 16:36:30 2020
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
-from pyRegression.nonlinear_regression import calc_function
 
 from pyAnalytics.hplc_data import hplc_data
 
 
 class hplc_calibration():
-    def __init__(self, data_mode, **kwargs):
+    def __init__(self, data_mode, time_limits=None, wavelength_limits=None,
+                 **kwargs):
         if data_mode == 'DataFrame':
             calibration_data = kwargs.get('calibration_data')
             self.concentrations = kwargs.get('concentrations')[np.newaxis]
@@ -25,90 +23,36 @@ class hplc_calibration():
                 self.calibration_data.append(
                         hplc_data('DataFrame', data=curr_data))
         else:
-            raise ValueError('No valid mode given.')
-        
-        self.wavelengths = self.calibration_data[0].wavelengths
-        self.calibration_integrated = np.empty((len(self.wavelengths),
-                                                len(np.squeeze(self.concentrations))))
+            raise ValueError('No valid data_mode given.')
+
+        self.time_limits = time_limits
+        self.wavelength_limits = wavelength_limits
+        self.calculate_calibration()
+            
+    def calculate_calibration(self, time_limits=None, wavelength_limits=None):
+        if time_limits is not None:
+            self.time_limits = time_limits
+        if wavelength_limits is not None:
+            self.wavelength_limits = wavelength_limits
+
+        self.calibration_integrated = []
         for index, curr_data in enumerate(self.calibration_data):
-            self.calibration_integrated[:, index] = curr_data.integrate_all_elugrams()
-            
-        self.K = None
-            
-    def calibration(self, mode):
-        if mode == 'multivariate':
-            self.K = np.dot(np.dot(self.calibration_integrated, self.concentrations.T), np.linalg.inv(np.dot(self.concentrations, self.concentrations.T)))
-        elif mode == 'univariate':
-            pass # not yet integrated
-        else:
-            raise ValueError('No valid calibration mode entered.')
+            self.calibration_integrated.append(
+                    curr_data.integrate_all_data(
+                            time_limits=self.time_limits,
+                            wavelength_limits=self.wavelength_limits))
+        self.calibration_integrated = pd.DataFrame(
+                self.calibration_integrated).T
+        
+        self.K = np.dot(
+                np.dot(
+                        self.calibration_integrated, self.concentrations.T
+                        ),
+                        np.linalg.inv(
+                                np.dot(
+                                        self.concentrations,
+                                        self.concentrations.T)
+                                ))
+        self.K = pd.DataFrame(self.K, index=self.calibration_integrated.index)
+
         return self.K
-    
-    def prediction(self, mode, sample):
-        if self.K is None:
-            self.calibration(mode)
-            
-        if mode == 'multivariate':
-            prediction = np.dot(
-                    np.linalg.inv(np.dot(self.K.T,self.K)),
-                    np.dot(self.K.T,
-                           sample.integrate_all_elugrams().values[np.newaxis].T))
-        elif mode == 'univariate':
-            pass # not yet integrated
-        else:
-            raise ValueError('No valid prediction mode entered.')
-            
-        return prediction
-
-# calculate simulated hplc calibration data
-wavenumbers = np.linspace(200,400,201)
-uv_spectrum = calc_function(wavenumbers, [4,200,0,15], 'Gauss') + calc_function(wavenumbers, [0.8,250,0,15], 'Gauss')
-
-times = np.linspace(0,10,1001)
-chromatogram = calc_function(times, [2.2,4.3,0,0.2], 'Gauss')
-
-concentrations = np.array([0.1, 0.2, 0.3, 0.4, 0.5])[np.newaxis]
-
-calibration = np.squeeze(concentrations)[:,np.newaxis,np.newaxis] * uv_spectrum * chromatogram[:,np.newaxis]
-noise = np.random.standard_normal(calibration.shape)*0.05
-calibration = calibration + noise
-
-calibration_dfs = []
-for curr_data in calibration:
-    calibration_dfs.append(pd.DataFrame(curr_data, index=times, columns=wavenumbers))
-    
-# generate hplc_calibration instance with simulated calibration data
-calibration = hplc_calibration('DataFrame', calibration_data=calibration_dfs, concentrations=np.squeeze(concentrations))
-
-K = calibration.calibration('multivariate')
-
-# classical_calibration = calibration_integrated[51, :]
-# slope_calibration = linregress(np.squeeze(concentrations),classical_calibration)[0]
-
-print('Korrekter Wert: 0.89400097')
-# print('Steigung klassische Kalibration:', slope_calibration)
-print('Steigung aus K-Matrix:', K[51])
-
-
-# calculate smaple with unknown concentration
-unknown_sample_1 = pd.DataFrame(
-        0.35 * uv_spectrum * chromatogram[:,np.newaxis], index=times, columns=wavenumbers)
-noise = np.random.standard_normal(unknown_sample_1.shape)*0.1
-unknown_sample_1 = unknown_sample_1 + noise
-unknown_sample_1 = hplc_data('DataFrame', data=unknown_sample_1)
-
-# predict sample concentration with simulated calibration data
-unknown_concentrations = calibration.prediction('multivariate', unknown_sample_1)
-
-# plot some data
-plt.figure(0)
-plt.plot(wavenumbers, calibration.calibration_data[0].raw_data.iloc[430, :])
-
-plt.figure(1)
-plt.plot(times, calibration.calibration_data[0].raw_data.iloc[:,51])
-
-plt.figure()
-plt.plot(wavenumbers, calibration.calibration_integrated)
-
-plt.figure()
-plt.plot(wavenumbers, K)
--- hplc_data.py
+++ hplc_data.py
@@ -7,7 +7,6 @@ Created on Wed Dec  4 14:26:41 2019
 
 import numpy as np
 import pandas as pd
-from scipy.integrate import cumtrapz
 
 ############### import own modules###############
 from pyAnalytics.measurement_parameters import measurement_parameters
@@ -23,7 +22,7 @@ class hplc_data:
                 self.directory = ''
             else:
                 self.directory = directory + '/'
-            
+
             self.import_from_file(self.file_name, self.directory)
         elif mode == 'DataFrame':
             self.raw_data = kwargs.get('data')
@@ -40,65 +39,119 @@ class hplc_data:
             self.PDA_data = measurement_parameters(measurement_params)
         else:
             raise ValueError('No valid mode entered. Allowed modes are \'import\' and \'DataFrame\'.')
-            
+
         self.time_data = self.raw_data.index.to_numpy()
-        
 
     def import_from_file(self, file_name, directory):
         # next two lines in case function is called after mode was not 'import'
         self.file_name = file_name
         self.directory = directory
-        
+
         PDA_data_raw = pd.read_csv(self.directory + self.file_name, sep='\t', skiprows=13, nrows=7)
         self.PDA_data = measurement_parameters(PDA_data_raw, parameter_names = ['interval','start_time','end_time','start_wavelength','end_wavelength','number_of_wavelength_points','number_of_time_points'])
         self.PDA_data.number_of_wavelength_points = int(self.PDA_data.number_of_wavelength_points)
         self.PDA_data.number_of_time_points = int(self.PDA_data.number_of_time_points)
-        
+
         self.wavelengths= np.linspace(self.PDA_data.start_wavelength,self.PDA_data.end_wavelength,num = self.PDA_data.number_of_wavelength_points)
-        
+
         self.raw_data = np.loadtxt(self.directory + self.file_name,skiprows=23,delimiter='\t')
         self.raw_data = pd.DataFrame(self.raw_data)
         self.raw_data.set_index(0,inplace=True)
         self.raw_data.columns = self.wavelengths
 
-    def extract_elugram(self,wavelength,time_limits = None,baseline_correction = False):
+    def crop_data(self, time_limits=None, wavelength_limits=None,
+                  active_data=None):
+        active_data = self.check_active_data(active_data)
+
         if time_limits is None:
             time_limits = [self.PDA_data.start_time, self.PDA_data.end_time]
-        else:
+        elif None in time_limits:
             if time_limits[0] is None:
-                time_limits = [self.PDA_data.start_time,time_limits[1]]
+                time_limits = [self.PDA_data.start_time, time_limits[1]]
             elif time_limits[1] is None:
-                time_limits = [time_limits[0],self.PDA_data.end_time]
-                
-        elugram = pd.Series(self.raw_data.iloc[self.closest_index_to_value(self.raw_data.index,time_limits[0]):
-                                               self.closest_index_to_value(self.raw_data.index,time_limits[1]),
-                                               self.closest_index_to_value(self.wavelengths,wavelength)],
-                            index = self.raw_data.index[self.closest_index_to_value(self.raw_data.index,time_limits[0]):
-                                                        self.closest_index_to_value(self.raw_data.index,time_limits[1])])
-        
-        if baseline_correction:
+                time_limits = [time_limits[0], self.PDA_data.end_time]
+
+        if wavelength_limits is None:
+            wavelength_limits = [self.PDA_data.start_wavelength, self.PDA_data.end_wavelength]
+        elif None in wavelength_limits:
+            if wavelength_limits[0] is None:
+                wavelength_limits = [self.PDA_data.start_wavelength, wavelength_limits[1]]
+            elif wavelength_limits[1] is None:
+                wavelength_limits = [wavelength_limits[0], self.PDA_data.end_wavelength]
+
+        cropped_data = active_data.iloc[
+                self.closest_index_to_value(
+                        active_data.index, time_limits[0]):
+                self.closest_index_to_value(
+                        active_data.index,time_limits[1]) + 1,
+                self.closest_index_to_value(
+                        active_data.columns, wavelength_limits[0]):
+                self.closest_index_to_value(
+                        active_data.columns, wavelength_limits[1]) + 1
+                ]
+
+        return cropped_data
+
+    def extract_elugram(self, wavelength, time_limits = None,
+                        active_data=None, baseline_correction = False):
+        active_data = self.check_active_data(active_data)
+
+        elugram = self.crop_data(
+                time_limits=time_limits,
+                wavelength_limits=[wavelength, wavelength],
+                active_data=active_data
+                )
+
+        #if baseline_correction:
             #baseline is not saved in any form and so far no method for full baseline correction exists
-            baseline = pd.Series(pyPreprocessing.baseline_correction.drPLS_baseline(elugram.values[np.newaxis,:],100000000,0.8,100)[0,:].T,index = elugram.index)
-            elugram = elugram - baseline
-            
-            
+        #    baseline = pd.Series(pyPreprocessing.baseline_correction.drPLS_baseline(elugram.values[np.newaxis,:],100000000,0.8,100)[0,:].T,index = elugram.index)
+        #    elugram = elugram - baseline
+
         return elugram
-    
-    def integrate_elugram(self,wavelength,time_limits = None,baseline_correction = False):
-        curr_elugram = self.extract_elugram(wavelength,time_limits = time_limits,baseline_correction = baseline_correction)
-        elugram_integrated = pd.Series(cumtrapz(curr_elugram,x = curr_elugram.index,initial = 0,axis = 0),index = curr_elugram.index)
-        
+
+    def integrate_elugram(self, wavelength, time_limits=None,
+                          active_data=None, baseline_correction=False):
+        active_data = self.check_active_data(active_data)
+
+        curr_elugram = self.extract_elugram(wavelength,
+                                            time_limits=time_limits,
+                                            baseline_correction=
+                                            baseline_correction,
+                                            active_data=active_data)
+        elugram_integrated = np.trapz(curr_elugram, x=curr_elugram.index,
+                                      axis=0).item()
+
         return elugram_integrated
-    
-    def integrate_all_elugrams(self): # baseline_correction and time limits not yet integrated
-        integrated_elugrams = np.trapz(self.raw_data.values, x=self.time_data, axis=0)
-        return pd.Series(integrated_elugrams, index=self.wavelengths)
+
+    def integrate_all_data(self, mode='elugrams', time_limits=None,
+                           wavelength_limits=None, active_data=None): # baseline_correction not yet integrated
+        active_data = self.check_active_data(active_data)
+        active_data = self.crop_data(time_limits=time_limits,
+                                     wavelength_limits=wavelength_limits,
+                                     active_data=active_data)
         
-    #next function should not stay in object
-    def closest_index_to_value(self,array,value):
+        if mode == 'elugrams':
+            integration_axis = 0
+            x_data = active_data.index
+            result_index = active_data.columns
+        elif mode == 'spectra':
+            integration_axis = 1
+            x_data = active_data.columns
+            result_index = active_data.index
+        
+        integrated_data = np.trapz(active_data.values, x=x_data,
+                                   axis=integration_axis)
+        integrated_data = pd.Series(integrated_data,
+                                    index=result_index)
+        return integrated_data
+        
+    # next function should not stay in object
+    def closest_index_to_value(self, array, value):
         return np.argmin(np.abs(array - value))
         
-        
-
+    def check_active_data(self, active_data):
+        if active_data is None:
+            active_data = self.raw_data
+        return active_data
             
         
