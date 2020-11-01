01c15a6b36d0b1e8e26022056e4e51beb355d9be
Snijderfrey <peter.vandersnijderfrey@web.de>
2020-11-02 00:38:55 +0100

    Code cleanup

--- spectroscopy_data.py
+++ spectroscopy_data.py
@@ -2,60 +2,49 @@
 """
 Provides a class for spectroscopic data preprocessing and analysis.
 """
+
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
+from sklearn.preprocessing import scale
 from tqdm import tqdm
 from scipy.integrate import cumtrapz
 from sklearn.decomposition import PCA
 
-# import own packagaes ######################
-import pyPreprocessing.baseline_correction as baseline_correction
-import pyPreprocessing.smoothing as smooth_data
+from pyPreprocessing.baseline_correction import generate_baseline
+from pyPreprocessing.smoothing import smoothing as smooth_data
 import pyPreprocessing.transform as transform
 import pyRegression.linear_regression as l_reg
-#############################################
 
 
 class spectroscopy_data:
     def __init__(self, spectral_data):
+        """
+        Initialize the spectroscopy_data instance.
+
+        Parameters
+        ----------
+        spectral_data : pandas DataFrame
+            A pandas DataFrame containing the spectral information. Each line
+            contains one spectrum. The index may contain sample names, numbers,
+            or e.g. coordinates from Raman maps. The column index must be the
+            wavenumber/wavelength scale sorted in decreasing order.
+
+        Returns
+        -------
+        None.
+
+        """
         self.spectral_data = spectral_data
-
+        self.reset_processed_data()
         self.wavenumbers = self.spectral_data.columns.to_numpy()
+        self.baseline_data = {}
 
-        self.baseline_data = {
-            'SNIP': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'ALSS': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'iALSS': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'drPLS': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'ModPoly': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'IModPoly': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns),
-            'none': pd.DataFrame(
-                np.zeros_like(self.spectral_data.values),
-                index=self.spectral_data.index,
-                columns=self.spectral_data.columns)}
+    def reset_processed_data(self):
+        self.spectral_data_processed = self.spectral_data.copy()
 
     def check_active_spectra(self, active_spectra):
         if active_spectra is None:
-            active_spectra = self.spectral_data
+            active_spectra = self.spectral_data_processed
         return active_spectra
 
 ###############################
@@ -63,23 +52,95 @@ class spectroscopy_data:
 ###############################
 
     def mean_center(self, active_spectra=None):
+        """
+        Subtract the mean spectrum from each spectrum im self.spectral_data.
+
+        Parameters
+        ----------
+        active_spectra : pandas DataFrame or None, optional
+            None means that self.spectral_data_processed is used for baseline
+            correction. Alternatively, a pandas DataFrame in the same format as
+            self.spectral_data_processed can be passed that is used for the
+            calculations. The default is None.
+
+        Returns
+        -------
+        pandas DataFrame
+            The mean centered spectral data.
+
+        """
         active_spectra = self.check_active_spectra(active_spectra)
 
         mean_centered_data = pd.DataFrame(
-            preprocessing.scale(active_spectra, axis=0, with_std=False),
-            index=active_spectra.index, columns=active_spectra.columns)
-        return mean_centered_data.round(decimals=6)
+            scale(active_spectra, axis=0, with_std=False),
+            index=active_spectra.index, columns=active_spectra.columns).round(
+                decimals=6)
+
+        self.spectral_data_processed = mean_centered_data
+
+        return mean_centered_data
 
     def standard_normal_variate(self, active_spectra=None):
+        """
+        Mean center spectra and scale to unit variance (SNV).
+
+        Mean centering is perforemd by subtracting the mean spectrum from each
+        spectrum. Scaling to unit variance is done by calculating the standard
+        deviation at each wavenumber and subsequent division of each intensity
+        by the respective standard deviation at that wavenumber. The resulting
+        dataset has a mean of zero and a standrad deviation of one at each
+        wavenumber.
+
+        Parameters
+        ----------
+        active_spectra : pandas DataFrame or None, optional
+            None means that self.spectral_data_processed is used for baseline
+            correction. Alternatively, a pandas DataFrame in the same format as
+            self.spectral_data_processed can be passed that is used for the
+            calculations. The default is None.
+
+        Returns
+        -------
+        pandas DataFrame
+            The mean centered and scaled spectral data.
+
+        """
         active_spectra = self.check_active_spectra(active_spectra)
 
         SNV_scaled_data = pd.DataFrame(active_spectra.subtract(
             active_spectra.mean(axis=1), axis=0).divide(
                 active_spectra.std(axis=1), axis=0),
-                index=active_spectra.index, columns=active_spectra.columns)
-        return SNV_scaled_data.round(decimals=6)
+                index=active_spectra.index,
+                columns=active_spectra.columns).round(decimals=6)
+
+        self.spectral_data_processed = SNV_scaled_data
+
+        return SNV_scaled_data
 
     def clip_wavenumbers(self, wn_limits, active_spectra=None):
+        """
+        Select certain wavenumber ranges from spectral data.
+
+        Parameters
+        ----------
+        wn_limits : list of tuples
+            A list containing tuples with two numbers. Each tuple gives a lower
+            and an upper wavenumber defining a wavenumber range. When more than
+            one tuple is given, the remaining spectral regions are not
+            necessarily neighboring spectral regions in the original data.
+        active_spectra : pandas DataFrame or None, optional
+            None means that self.spectral_data_processed is used for baseline
+            correction. Alternatively, a pandas DataFrame in the same format as
+            self.spectral_data_processed can be passed that is used for the
+            calculations. The default is None.
+
+        Returns
+        -------
+        clipped_data : pandas DataFrame
+            The spectral regions collected in one DataFrame defined by the
+            wn_limits.
+
+        """
         active_spectra = self.check_active_spectra(active_spectra)
 
         wn_limits = np.array(wn_limits)
@@ -104,10 +165,17 @@ class spectroscopy_data:
                  for ii in np.arange(len(closest_index_to_lower_wn))]))
 
         clipped_data = active_spectra.iloc[:, clipping_index]
+
+        self.spectral_data_processed = clipped_data
+
         return clipped_data
 
     def clip_samples(self, x_limits=None, y_limits=None, z_limits=None,
                      active_spectra=None):
+        # This method is not good for this class. It assumes that the index
+        # contains info only present for Raman images, so it must be improved
+        # to accept all kinds on index values, such as sample names or simply
+        # numbers.
         active_spectra = self.check_active_spectra(active_spectra)
 
         x_clipping_mask = self.generate_sample_clipping_mask(active_spectra,
@@ -119,24 +187,20 @@ class spectroscopy_data:
 
         clipped_data = active_spectra.loc[(x_clipping_mask, y_clipping_mask,
                                            z_clipping_mask)]
+
+        self.spectral_data_processed = clipped_data
+
         return clipped_data
-
-    def integrate_image(self, active_spectra=None):
-        active_spectra = self.check_active_spectra(active_spectra)
-
-        hyperspectral_image_integrated = pd.DataFrame(
-            -cumtrapz(active_spectra, x=active_spectra.columns+0, initial=0),
-            index=active_spectra.index, columns=active_spectra.columns
-            ).round(decimals=6)
-        return hyperspectral_image_integrated
 
     def smoothing(self, mode, active_spectra=None, **kwargs):
         active_spectra = self.check_active_spectra(active_spectra)
 
-        smoothed_data = pd.DataFrame(smooth_data.smoothing(
-            active_spectra.values, mode=mode, **kwargs),
-            index=active_spectra.index,
-            columns=active_spectra.columns).round(decimals=6)
+        smoothed_data = pd.DataFrame(
+            smooth_data(active_spectra.values, mode=mode, **kwargs),
+            index=active_spectra.index, columns=active_spectra.columns).round(
+                decimals=6)
+
+        self.spectral_data_processed = smoothed_data
 
         return smoothed_data
 
@@ -147,56 +211,61 @@ class spectroscopy_data:
         assert mode in normalize_modes, 'normalize mode unknown'
 
         if mode == normalize_modes[0]:  # total_intensity
-            normalized_data = pd.DataFrame(transform.normalize(
+            normalized_data = pd.DataFrame(-transform.normalize(
                 active_spectra.values, mode,
                 x_data=active_spectra.columns.to_numpy()),
                 index=active_spectra.index, columns=active_spectra.columns)
-            
-        return -normalized_data
 
-    # needs to be adapted to fit all baseline correction modes
-    def baseline_correction(self, lam=100, lam_1=100, p=0.01, eta=0.5,
-                            n_iter=100, conv_crit=0.001, active_spectra=None,
-                            alg='SNIP', wavenumbers=[], poly_order=5):
+        self.spectral_data_processed = normalized_data
+
+        return normalized_data
+
+    def baseline_correction(self, mode='ModPoly', smoothing=True,
+                            transform=False, active_spectra=None, **kwargs):
+        """
+        Correct baseline with methods from pyPreprocessing.baseline_correction.
+
+        The details about the function arguments and kwargs can be found in the
+        docstrings in pyPreprocessing.baseline_correction.
+
+        Parameters
+        ----------
+        mode : str
+            The baseline correction algorithm.
+        smoothing : bool, optional
+            True means the spectra are smoothed before baseline correction. The
+            default is True.
+        transform : bool, optional
+            True means the spectra are transformed before baseline correction.
+            The default is False.
+        active_spectra : pandas DataFrame or None, optional
+            None means that self.spectral_data is used for baseline correction.
+            Alternatively, a pandas DataFrame in the same format as
+            self.spectral_data can be passed that is used for the calculations.
+            The default is None.
+        **kwargs
+            All kwargs necessary for the respective baseline correction mode.
+            If wavenumbers are needed for the calculations, these do not need
+            to be given because they are already known. If they are passed
+            anyway, the passed values are ignored.
+
+        Returns
+        -------
+        pd.DataFrame
+            The spectral data with the subtracted baseline.
+
+        """
         active_spectra = self.check_active_spectra(active_spectra)
 
-        baseline_types = ['ALSS', 'iALSS', 'drPLS', 'SNIP', 'ModPoly',
-                          'IModPoly']
-        assert alg in baseline_types, 'baseline type unknown'
+        if mode in ['convex_hull', 'ModPoly', 'IModPoly', 'PPF', 'iALSS']:
+            kwargs['wavenumbers'] = self.active_spectra.columns.to_numpy()
 
-        if alg == baseline_types[0]:  # ALSS
-            self.baseline_data[alg] = pd.DataFrame(
-                baseline_correction.generate_baseline(
-                    active_spectra.values, baseline_types[0], lam=lam, p=p,
-                    n_iter=n_iter, conv_crit=conv_crit),
-                index=active_spectra.index, columns=active_spectra.columns)
-        elif alg == baseline_types[1]:  # iALSS
-            self.baseline_data[alg] = pd.DataFrame(
-                baseline_correction.generate_baseline(
-                    active_spectra.values, baseline_types[1],
-                    wavenumbers=active_spectra.columns.to_numpy(), lam=lam,
-                    lam_1=lam_1, p=p, n_iter=n_iter, conv_crit=conv_crit),
-                index=active_spectra.index, columns=active_spectra.columns)
-        elif alg == baseline_types[2]:  # drPLS
-            self.baseline_data[alg] = pd.DataFrame(
-                baseline_correction.generate_baseline(
-                    active_spectra.values, baseline_types[2], lam=lam, eta=eta,
-                    n_iter=n_iter, conv_crit=conv_crit),
-                index=active_spectra.index, columns=active_spectra.columns)
-        elif alg == baseline_types[3]:  # SNIP
-            self.baseline_data[alg] = pd.DataFrame(
-                baseline_correction.generate_baseline(
-                    active_spectra.values, baseline_types[3], n_iter=n_iter),
-                index=active_spectra.index, columns=active_spectra.columns)
-        elif alg in baseline_types[4:6]:  # ModPoly, IModPoly
-            self.baseline_data[alg] = pd.DataFrame(
-                baseline_correction.generate_baseline(
-                    active_spectra.values, baseline_types[4],
-                    wavenumbers=wavenumbers, n_iter=n_iter,
-                    poly_order=poly_order),
-                index=active_spectra.index, columns=active_spectra.columns)
+        self.baseline_data[mode] = pd.DataFrame(
+            generate_baseline(active_spectra.values, mode, smoothing=True,
+                              tranform=False, **kwargs),
+            index=active_spectra.index, columns=active_spectra.columns)
 
-        corrected_data = active_spectra - self.baseline_data[alg]
+        corrected_data = active_spectra - self.baseline_data[mode]
         return corrected_data.round(decimals=6)
 
     def generate_sample_clipping_mask(self, active_spectra, limits, dimension):
@@ -256,6 +325,15 @@ class spectroscopy_data:
 # spectrum analysis methods
 ####################################
 
+    def integrate_spectra(self, active_spectra=None):
+        active_spectra = self.check_active_spectra(active_spectra)
+
+        spectra_integrated = pd.DataFrame(
+            -cumtrapz(active_spectra, x=active_spectra.columns+0, initial=0),
+            index=active_spectra.index, columns=active_spectra.columns
+            ).round(decimals=6)
+        return spectra_integrated
+
     def principal_component_analysis(self, pca_components,
                                      active_spectra=None):
         active_spectra = self.check_active_spectra(active_spectra)
@@ -303,6 +381,7 @@ class spectroscopy_data:
 ####################################
 # export methods
 ####################################
+
     def export_spectra(self, export_path, export_name, active_spectra=None):
         active_spectra = self.check_active_spectra(active_spectra)
 
