74d16c779e36e3e2d4bec0fafdd879c75e305d18
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-12-01 20:17:28 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pyAnalytics',
     version='0.0.1',
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels']
+    install_requires=['numpy', 'matplotlib', 'pandas', 'tqdm', 'imageio', 'scikit-learn', 'scipy', 'pip', 'plotly', 'little_helpers', 'pyPreprocessing', 'pyDataFitting', 'spc']
 )
