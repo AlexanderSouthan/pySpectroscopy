e44211e6b7b32c1382ec049401d4d6bb1016e960
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-02-09 14:25:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyAnalytics',
+    name='pySpectroscopy',
     version='0.0.1',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'matplotlib', 'pandas', 'tqdm', 'imageio', 'scikit-learn', 'scipy', 'pip', 'plotly', 'little_helpers', 'pyPreprocessing', 'pyDataFitting', 'spc']
