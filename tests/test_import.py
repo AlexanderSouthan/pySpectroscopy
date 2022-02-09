989cbbfb437ae11b16673483c5a50e06d1c899e7
Southan <alexander.southan@igb.fraunhofer.de>
2022-02-09 16:02:13 +0100

    Added simplest possible test

---
+++ tests/test_import.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 19:55:00 2021
+
+@author: Alexander Southan
+"""
+
+import unittest
+
+from src.pySpectroscopy.spectroscopy_data import spectroscopy_data
+
+
+class TestSpctroscopyData(unittest.TestCase):
+
+    def test_spectroscopy_data(self):
+
+        pass
